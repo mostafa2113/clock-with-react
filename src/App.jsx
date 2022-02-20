69d521283f2083b2ba5d9fe722f0d43833c0d881
mostafa <ace13520@gmail.com>
2022-02-20 08:46:37 +0200

    this my react clock to practice

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React,{useState} from 'react';
 import styles from './app.module.css'
 function App() {
   const [timeNow,setTimeNow] = useState(new Date());
-  setTimeout(() => {
+  setInterval(() => {
     setTimeNow(new Date());
   }, 1000);
   return (
     <>
       <div className={styles.timeNow}>
-        {timeNow.getHours()}:{timeNow.getMinutes()}:{timeNow.getSeconds()}
+        {timeNow.toLocaleTimeString()}
       </div>
     </>
   );
